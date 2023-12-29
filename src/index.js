b76d09cf67071893b68b9a0bd14016c368c5ef9b
Thamilkkumaran <105854249+sthamilkumaran@users.noreply.github.com>
2023-12-29 23:07:15 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Provider} from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from 'react-router-dom'
 import store from './redux/store';
 
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
-import { applyMiddleware, createStore} from "redux-thunk";
+import { applyMiddleware} from "redux";
+import { legacy_createStore as createStore } from 'redux'
 import logger from "redux-logger";
 import reduxThunk from "redux-thunk";
 import rootReducer from "./root-reducer";
+//import { default } from "redux-thunk";
 
 const middlewares = [reduxThunk];
 
@@ -9,7 +11,7 @@ if(process.env.NODE_ENV === "development") {
     middlewares.push(logger)
 }
 
-const store =  createStore(rootReducer,applyMiddleware(...middlewares));
+const store =  createStore(rootReducer, applyMiddleware(...middlewares));
 
 
 export default store;
