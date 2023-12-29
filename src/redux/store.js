f91b7e3679089b5adad5749e8c805fcbdccb6df8
Thamilkkumaran <105854249+sthamilkumaran@users.noreply.github.com>
2023-12-29 20:15:32 +0530

    react crud update

---
+++ src/redux/store.js
@@ -0,0 +1,15 @@
+import { applyMiddleware, createStore} from "redux-thunk";
+import logger from "redux-logger";
+import reduxThunk from "redux-thunk";
+import rootReducer from "./root-reducer";
+
+const middlewares = [reduxThunk];
+
+if(process.env.NODE_ENV === "development") {
+    middlewares.push(logger)
+}
+
+const store =  createStore(rootReducer,applyMiddleware(...middlewares));
+
+
+export default store;
